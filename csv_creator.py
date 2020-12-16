3e1446469cdadf1d3293c7c286bb4b53ae2906f9
Giovanni Fabris <giovanni.fabris@student.h-farm.com>
2020-12-16 18:03:33 +0100

    solved csv issue

---
+++ csv_creator.py
@@ -0,0 +1,22 @@
+import csv
+from list_of_guitarists import list_of_guitarists
+import pandas as pd
+
+def database(dictionary, name_csv="players_bands.csv"):
+
+    """
+    the following function takes the elements present
+    in the dictionary and places them into a csv file
+    named guitarists.csv that we will use as our database.
+    We will be able to add new bands and guitarists then.
+    """
+
+    g_b = []
+    for key, value in dictionary.items():
+        g_b.append([key,value])
+    df = pd.DataFrame(g_b, columns=["Players","Bands"])
+    df.to_csv(name_csv)
+    return print("database created successfully ")
+    
+
+database(list_of_guitarists)
